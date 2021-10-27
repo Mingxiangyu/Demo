1970b9455c844cd2d847e64355eee6cc4c61bb0f
mingxiangyu <mxiang_yu@163.com>
2021-10-27 09:53:21 +0800

    添加minIO

---
+++ varietyOfTools/src/main/java/org/demo/minIO/MinIO上传文件.java
@@ -0,0 +1,55 @@
+package org.demo.minIO;
+
+import io.minio.MinioClient;
+import io.minio.Result;
+import io.minio.errors.MinioException;
+import io.minio.messages.Item;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import org.xmlpull.v1.XmlPullParserException;
+
+public class MinIO上传文件 {
+
+  public static void main(String[] args)
+      throws NoSuchAlgorithmException, IOException, InvalidKeyException, XmlPullParserException {
+    try {
+      // 使用MinIO服务的URL，端口，Access key和Secret key创建一个MinioClient对象
+      MinioClient minioClient =
+          new MinioClient(
+              "https://play.min.io",
+              "Q3AM3UQ867SPQQA43P2F",
+              "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG");
+
+      // 列出所有存储桶
+      //      List<Bucket> bucketList = minioClient.listBuckets();
+      //      for (Bucket bucket : bucketList) {
+      //        System.out.println(bucket.creationDate() + ", " + bucket.name());
+      //      }
+
+      // 列出'my-bucketname'里的对象
+      Iterable<Result<Item>> myObjects = minioClient.listObjects("10-26");
+      for (Result<Item> result : myObjects) {
+        Item item = result.get();
+        System.out.println(item.lastModified() + ", " + item.size() + ", " + item.objectName());
+      }
+
+      // 检查存储桶是否已经存在
+      boolean isExist = minioClient.bucketExists("10-26");
+      if (isExist) {
+        System.out.println("Bucket already exists.");
+      } else {
+        // 创建一个名为asiatrip的存储桶，用于存储照片的zip文件。
+        minioClient.makeBucket("10-26");
+      }
+
+      // 使用putObject上传一个文件到存储桶中。
+      minioClient.putObject(
+          "10-26", "/中午14/ces.docx", "C:\\Users\\T480S\\Desktop\\ces.docx", null);
+      System.out.println(
+          "/home/user/Photos/asiaphotos.zip is successfully uploaded as asiaphotos.zip to `asiatrip` bucket.");
+    } catch (MinioException e) {
+      System.out.println("Error occurred: " + e);
+    }
+  }
+}
